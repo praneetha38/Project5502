a2556b0e37d5ee1bd714ae0644992010a1b06db1
praneetha38 <139118699+praneetha38@users.noreply.github.com>
2024-12-13 12:50:57 -0600

    Add files via upload

---
+++ production/functions.py
@@ -0,0 +1,11 @@
+import pickle
+import pandas as pd
+
+with open('production/models/model_v1.0.0.pkl', 'rb') as file:
+    model = pickle.load(file)
+
+def predict_subscribe(watch, duration, ctr, interest):
+    data = [{"watch_time": watch, "avg_view_duration": duration, "click_through_rate": ctr, "interest": interest}]
+    sample = pd.DataFrame(data)
+    pred = model.predict_proba(sample.values)[0]
+    return {'Misses Out': pred[0], 'Subscribes': pred[1]}
